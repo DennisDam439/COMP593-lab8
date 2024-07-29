3d41571c280665dc82a937c68da05687ee3246b7
DennisDam439 <dend@flemingcollege.ca>
2024-07-29 11:57:40 -0400

    assignment to populate

--- create_relationships.py
+++ create_relationships.py
@@ -8,18 +8,39 @@ Usage:
 """
 import os
 
+
 # Determine the path of the database
 script_dir = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(script_dir, 'social_network.db')
 
 def main():
-    create_relationships_table()
+    create_relationships_table(db_path)
     populate_relationships_table()
 
-def create_relationships_table():
+def create_relationships_table(path):
     """Creates the relationships table in the DB"""
-    # TODO: Function body
-    # Hint: See example code in lab instructions entitled "Add the Relationships Table"
+
+    con = sqlite3.connect(path) 
+    cur = con.cursor()  
+
+    create_relationships_tbl_query = """ 
+    CREATE TABLE IF NOT EXISTS relationships 
+    ( 
+        id         INTEGER PRIMARY KEY,         
+        person1_id INTEGER NOT NULL,
+        person2_id INTEGER NOT NULL,         
+        type       TEXT NOT NULL,         
+        start_date DATE NOT NULL, 
+        FOREIGN KEY (person1_id) REFERENCES people (id), 
+        FOREIGN KEY (person2_id) REFERENCES people (id) 
+    ); 
+    """  
+    # Execute the SQL query to create the 'relationships' table. cur.execute(create_relationships_tbl_query) 
+    cur.execute(create_relationships_tbl_query) 
+    con.commit() 
+    con.close() 
+
+
     return
 
 def populate_relationships_table():
---
+++ test.py
@@ -0,0 +1,6 @@
+a = 15
+
+def _math(number=10):
+    print(number)
+
+_math(a)
