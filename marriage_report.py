2b769425ca68170feb031ece9d1f6c80a00f8118
DennisDam439 <dend@flemingcollege.ca>
2024-08-05 15:18:04 -0400

    Update marriage_report.py

--- marriage_report.py
+++ marriage_report.py
@@ -23,30 +23,25 @@ def get_married_couples():
     """Queries the Social Network database for all married couples.
 
     Returns:
-        list: (person1, person2, start_date) of married couples 
+        list: (name1, name2, start_date) of married couples 
     """
     # TODO: Function body
     # Hint: See example code in lab instructions entitled "Get a List of Relationships"
-    db_path = "C:\Users\dddam\OneDrive\Desktop\githubrepo\COMP593-lab8\social_network.db"
     con = sqlite3.connect(db_path)
-    cur =con.cursor()
-    cur.execute("SELECT  person1, person2, start_date FROM relationships WHERE type='married'")
-    married_couples = cur.fetchall()
-    con.close()
-    return married_couples 
-
+    cursor = con.cursor()
+   
 def save_married_couples_csv(married_couples, csv_path):
     """Saves list of married couples to a CSV file, including both people's 
     names and their wedding anniversary date  
 
     Args:
-        married_couples (list): (person1, person2, start_date) of married couples
+        married_couples (list): (name1, name2, start_date) of married couples
         csv_path (str): Path of CSV file
     """
     # TODO: Function body
     # Hint: We did this in Lab 7.
-    df = pd.DataFrame(married_couples, columns= ['Person 1', 'Person 2', 'Wedding Anniversary'])
-    df.to_csv(csv_path, index= False)
+    df = pd.DataFrame(married_couples, colums=['Name 1', 'Name 2', 'Wedding Anniversary' ])
+    return
 
 if __name__ == '__main__':
    main()
